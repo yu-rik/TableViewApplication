5a003844cff879ba2b5080a6ffcb6fc7185772ec
Yurii Nosachenko <48627510+yu-rik@users.noreply.github.com>
2020-06-03 19:36:01 +0300

    Added start's files

---
+++ UITableApp/UITableApp/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableApp
+//
+//  Created by yurik on 6/3/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
