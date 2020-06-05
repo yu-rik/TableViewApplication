05a76049431a76067e1d2a75ae3039ea35748a26
Yurii Nosachenko <yurik_n@outlook.com>
2020-06-05 14:49:40 +0300

    adde new class for Cell

---
+++ UITableApp/UITableApp/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  UITableApp
+//
+//  Created by yurik on 6/5/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+
+
+}
