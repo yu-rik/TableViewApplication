fc87897e3c04a2ad9f72072022f6af8cb8aef085
Yurii Nosachenko <yurik_n@outlook.com>
2020-06-06 01:16:34 +0300

    Added model of places

--- UITableApp/UITableApp/MainViewController.swift
+++ UITableApp/UITableApp/MainViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 
 class MainViewController: UITableViewController {
 
-    let restaurantsName = ["Puzata hata","Napaleon hill","Big mac","Stolovaya #5","Uzbek","Proreznaya","Ludmila","Oaza","Partenit","Semeiz","Vepr",
-                           "Velyka bochka","Praga","London","Toronto"]
+
     
+    let places = Place.getPlaces()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +23,16 @@ class MainViewController: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return restaurantsName.count
+       return places.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Jacheyka", for: indexPath) as! CustomTableViewCell
-        cell.nameLabel.text = restaurantsName[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantsName[indexPath.row])
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = (cell.imageOfPlace.frame.size.height)/2 //cell.frame.size.height -значение высоты ячейки родительского класса UITableViewCell
         cell.imageOfPlace.clipsToBounds = true
         
@@ -38,12 +40,12 @@ class MainViewController: UITableViewController {
         return cell
     }
     
-    // MARK: - TableViewDelegate
-     //метод возвращает высоту строки
+   /* // MARK: - TableViewDelegate
+     //метод возвращает высоту строки, если выcоту строки указывать в storyboard, то этот метод не нужен
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 85
     }
-    
+    */
     
    /*
     // MARK: - Navigation
---
+++ UITableApp/UITableApp/PlaceModel.swift
@@ -0,0 +1,25 @@
+//
+//  PlaceModel.swift
+//  UITableApp
+//
+//  Created by yurik on 6/6/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import Foundation
+struct Place {
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+    
+     static let restaurantsName = ["Puzata hata","Napaleon hill","Big mac","Stolovaya #5","Uzbek","Proreznaya","Ludmila","Oaza","Partenit","Semeiz","Vepr", "Velyka bochka","Praga","London","Toronto"]
+    
+    static func getPlaces() ->[Place]{
+        var places = [Place]()
+        for place in restaurantsName {
+            places.append(Place(name: place, location: "Киев", type: "Ресторан", image: place))
+        }
+        return places
+    }
+}
