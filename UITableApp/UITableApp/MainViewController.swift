c7d85f7b8d201946c3ff90dcb656db7cd1c764cd
Yurii Nosachenko <yurik_n@outlook.com>
2020-06-06 15:20:19 +0300

    Create add window's

--- UITableApp/UITableApp/MainViewController.swift
+++ UITableApp/UITableApp/MainViewController.swift
@@ -56,5 +56,7 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue){}
 
 }
