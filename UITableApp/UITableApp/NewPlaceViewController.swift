12a0ecf81a16b3dff4f88461e67a6d02b6bf920b
Yurii Nosachenko <yurik_n@outlook.com>
2020-06-09 12:53:03 +0300

    Create methods for work with images on the phone and work with images of camera

--- UITableApp/UITableApp/NewPlaceViewController.swift
+++ UITableApp/UITableApp/NewPlaceViewController.swift
@@ -17,15 +17,41 @@ class NewPlaceViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0{
+            let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)
+            }
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            present(actionSheet, animated: true)
             
         }else{
             view.endEditing(true)
         }
     }
+   //MARK: Work with Image
+    func chooseImagePicker(source: UIImagePickerController.SourceType){
+        if UIImagePickerController.isSourceTypeAvailable(source){
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true //позволяет редактировать фото пользователю
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+            
+        }
+    }
 }
+//MARK: TextFieldDelegate
 extension NewPlaceViewController : UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
 }
+
+
